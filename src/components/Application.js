804b1dd110b22d18ff9a8fb455e36d5a0d5e02fb
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2022-11-08 17:05:50 -0600

    replace the hardcoded days data with api connection

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,25 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
+import axios from "axios";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const [days, setDays] = useState([]);
 
 const appointments = {
   "1": {
@@ -70,8 +55,13 @@ const appointments = {
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
 
-  // console.log(day);
+  useEffect(() => {
+    axios.get('/api/days').then(response => {
+      setDays(response.data);
+    });
+  }, []);
 
   return (
     <main className="layout">
