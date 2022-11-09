140d3e0d1b63451f074e2532785630966ca23a13
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2022-11-09 13:48:28 -0600

    tapped into api for days and appointments info

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
+import { getAppointmentsForDay } from "helpers/selectors";
 import axios from "axios";
 
 export default function Application(props) {
@@ -13,11 +14,17 @@ export default function Application(props) {
   });
 
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   useEffect(() => {
-    axios.get('/api/days').then(response => {
-      setDays(response.data);
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments')
+      // axios.get('http://localhost:8001/api/interviewers')
+    ]).then((all) => {
+      // console.log("+++++++++++", all);
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
     });
   }, []);
 
@@ -40,9 +47,9 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {Object.values(state.appointments).map(appointment => <Appointment
-          key={appointment.id}
-          {...appointment}
+        {dailyAppointments.map(appointments => <Appointment
+          key={appointments.id}
+          {...appointments}
         />)}
       </section>
     </main>
