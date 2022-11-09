7d1b543e812a3c0b69ad27f4c2a6a52d02bf7365
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2022-11-08 18:38:28 -0600

    Jest test pass

---
+++ src/helpers/selectors.js
@@ -0,0 +1,19 @@
+
+export function getAppointmentsForDay(state, day) {
+
+  const dayAppointments = [];
+
+  const filterDay = state.days.find(selectedDay => selectedDay.name === day);
+
+  if (!filterDay) {
+    return [];
+  }
+
+  for (const appointmentID of filterDay.appointments) {
+    if (state.appointments[appointmentID]) {
+      dayAppointments.push(state.appointments[appointmentID])
+    }
+  }
+  return dayAppointments; 
+
+}
