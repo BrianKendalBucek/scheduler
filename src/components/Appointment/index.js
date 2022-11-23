54d22187c45a09858af43e58a2460fbd0618dc3b
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2022-11-23 13:20:13 -0600

    implimented Validate function, and set up tests, all passing

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -53,8 +53,8 @@ export default function Appointment(props) {
     {mode === EMPTY && <Empty
       onAdd={() => { transition(CREATE) }} />}
     {mode === SHOW && <Show
-      student={props.interview?.student}
-      interviewer={props.interview?.interviewer}
+      student={props.interview.student}
+      interviewer={props.interview.interviewer}
       onDelete={() => { transition(CONFIRM) }}
       onEdit={() => { transition(EDIT) }} />}
     {mode === CREATE && <Form
